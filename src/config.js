e802006e49bc1e4bdfbfc3b5bed8d4ff9220c516
nbvnbv <nbv3nbv@yahoo.com>
2024-06-12 13:37:02 +0300

    change crossDomainPort

--- src/config.js
+++ src/config.js
@@ -27,8 +27,9 @@ module.exports = {
     // from a reverse proxy.
     //getServerInfo: () => ({ hostname: '0.0.0.0', port: 8000, crossDomainPort: 8081, protocol: 'http:' }),
     // example of non-hard-coding the hostname header
+    // crossDomainPort: 8443
     getServerInfo: (req) => {
-         return { hostname: new URL('http://' + req.headers.host).hostname, port: 443, crossDomainPort: 8443, protocol: 'https:' };
+         return { hostname: new URL('http://' + req.headers.host).hostname, port: 443, crossDomainPort: 8081, protocol: 'https:' };
     },
 
     // enforce a password for creating new sessions. set to null to disable
